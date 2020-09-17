160638cbaea0af05a369ca45bb61cfb33e68f529
David <dops.t00@gmail.com>
2020-09-17 00:39:14 -0300

    applying learned techniques

--- src/App.js
+++ src/App.js
@@ -6,50 +6,60 @@ import {generate as id} from 'shortid';
 import Card from './components/Card.jsx';
 
 
-const cards = [
-  {
-  id: id(),
-  title: "Card 1",
-  img: '/assets/wolf.jpg',
-  author: 'Dorian',
-  date: new Date().toLocaleDateString(),
-  tags: ['animal', 'wolf', 'snow'],
-  views: 23,
-  description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
-  },
-  {
-  id: id(),
-  title: "Card 2",
-  img: '/assets/wolf.jpg',
-  author: 'Dorian',
-  date: new Date().toLocaleDateString(),
-  tags: ['animal', 'wolf', 'snow'],
-  views: 24,
-  description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
-  },
-  {
-  id: id(),
-  title: "Card 3",
-  img: '/assets/wolf.jpg',
-  author: 'Dorian',
-  date: new Date().toLocaleDateString(),
-  tags: ['animal', 'wolf', 'snow'],
-  views: 25,
-  description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
-  }
-]
-
-
 function App() {
 
+  const cards = [
+    {
+    id: id(),
+    title: "Card 1",
+    img: '/assets/wolf.jpg',
+    author: 'Dorian',
+    date: new Date().toLocaleDateString(),
+    tags: ['animal', 'wolf', 'snow'],
+    views: 23,
+    description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    },
+    {
+    id: id(),
+    title: "Card 2",
+    img: '/assets/wolf.jpg',
+    author: 'Dorian',
+    date: new Date().toLocaleDateString(),
+    tags: ['animal', 'wolf', 'snow'],
+    views: 24,
+    description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    },
+    {
+    id: id(),
+    title: "Card 3",
+    img: '/assets/wolf.jpg',
+    author: 'Dorian',
+    date: new Date().toLocaleDateString(),
+    tags: ['animal', 'wolf', 'snow'],
+    views: 25,
+    description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    }
+  ]
+ 
   return (
 
       <div key="cardsContainer" className="cards">
-    
-          {
-            cards.map(
-              data => ( <Card key={id()} info={data} /> )
-            )
+        {
+          cards.map(data => ( 
+              <Card                  
+               key={data.id}
+               title={data.title} 
+               img={data.img}
+               author={data.author}
+               date={data.date}
+               tags={data.tags}
+               views={data.views}
+              >
+
+              {data.description}
+              
+              </Card> 
+          ))
         }
       </div>  
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import {generate as id} from 'shortid';
 
-const Card = ({info})=>{
+const Card = ({title,img,author,tags,views,date,children})=>{
 
 	return (
 		<>	
-			<div className="card" key={info.id} >
-				<h2 className="card__title">{info.title}</h2>
-				<img src={info.img} alt='wolf_picture' className="card__img" />
-				<p className="card__description">{info.description}</p>
-				<p className="card__author">{info.author}</p>
+			<div className="card">
+				<h2 className="card__title">{title}</h2>
+				<img src={img} alt='wolf_picture' className="card__img" />
+				<p className="card__description">{children}</p>
+				<p className="card__author">{author}</p>
 				<ul className="card__tags">
-					{info.tags.map((tags, key) => (<li className="card__tag" key={id()}>{info.tags[key]}</li>) )}
+					{
+						tags.map( tag => (
+										<li className="card__tag" key={id()}>{tag}</li>
+									) 
+						)
+					}
 				</ul>
-				<p className="card__views">{info.views} Views</p>
-				<p className="card__date">{info.date}</p>
+				<p className="card__views">{views} Views</p>
+				<p className="card__date">{date}</p>
 			</div>
 		</>
 
-
-		);
+	);
 
 }
 
